f5a68d879acc3556eab5ef524da20d1ef69a4b8e
Himagiri Nandan <himagirinandantuduru@gmail.com>
2025-08-15 02:47:02 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express"
 import dotenv from "dotenv";
 import rateLimiter from "./middleware/rateLimiter.js";
 import transactionsRoute from "./routes/transactionsRoute.js";
+import cors from "cors";
 import { initDB } from "./config/db.js";
 dotenv.config();
 
@@ -9,9 +10,16 @@ const app = express();
 app.use(express.json());
 app.use(rateLimiter);
 
+app.use(
+  cors({
+    credentials: true, 
+    origin: ["https://rn-wallet-frontend.vercel.app"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
 const port = process.env.PORT || 5001;
-
-
 
 app.use("/api/transactions", transactionsRoute);
 
